6e77b8a486c2fd952cbf95db9350da7da9938e6a
Eileen <devcraft10@devcraft10s-iMac.local>
2017-01-11 16:24:57 +0900

    temp commit

--- android/app/src/main/java/io/harry/zealot/activity/MenuActivity.java
+++ android/app/src/main/java/io/harry/zealot/activity/MenuActivity.java
@@ -1,7 +1,12 @@
 package io.harry.zealot.activity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 
 import butterknife.ButterKnife;
@@ -9,6 +14,8 @@ import butterknife.OnClick;
 import io.harry.zealot.R;
 
 public class MenuActivity extends ZealotBaseActivity {
+    public static final int PICK_PHOTO = 9;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,28 @@ public class MenuActivity extends ZealotBaseActivity {
     @OnClick(R.id.start_button)
     public void onStartClick() {
         startActivity(new Intent(this, TestAjaeActivity.class));
+    }
+
+    @OnClick(R.id.upload_button)
+    public void onUploadClick() {
+        startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), PICK_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri selectedImage = data.getData();
+        Cursor cursor = getContentResolver().query(selectedImage, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+        String decodableImage = "";
+        if(cursor.moveToFirst()) {
+            decodableImage = cursor.getString(0);
+        }
+
+        cursor.close();
+
+        Bitmap imageSource = BitmapFactory.decodeFile(decodableImage);
+        Bitmap.createScaledBitmap(imageSource, )
+
     }
 }
--- android/app/src/test/java/io/harry/zealot/activity/MenuActivityTest.java
+++ android/app/src/test/java/io/harry/zealot/activity/MenuActivityTest.java
@@ -1,5 +1,10 @@
 package io.harry.zealot.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+
 import org.assertj.android.api.content.IntentAssert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,15 +12,18 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import io.harry.zealot.BuildConfig;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.robolectric.RuntimeEnvironment.application;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class MenuActivityTest {
+    public static final int PICK_PHOTO = 9;
     private MenuActivity subject;
 
     @Before
@@ -29,5 +37,30 @@ public class MenuActivityTest {
 
         IntentAssert intentAssert = new IntentAssert(shadowOf(subject).getNextStartedActivity());
         intentAssert.hasComponent(application, TestAjaeActivity.class);
+    }
+
+    @Test
+    public void onUploadAjae_launchesGalleryAppToPickPhoto() throws Exception {
+        subject.onUploadClick();
+
+        ShadowActivity.IntentForResult actual = shadowOf(subject).getNextStartedActivityForResult();
+
+        assertThat(actual.requestCode).isEqualTo(PICK_PHOTO);
+
+        IntentAssert intentAssert = new IntentAssert(actual.intent);
+
+        intentAssert.hasAction(Intent.ACTION_PICK);
+        intentAssert.hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+    }
+
+    @Test
+    public void onActivityResult_createsBitmapImageFromURI() throws Exception {
+        Intent data = new Intent();
+        Uri uri = new Uri.Builder().build();
+        data.setData(uri);
+
+        subject.onActivityResult(PICK_PHOTO, Activity.RESULT_OK, data);
+        
+
     }
 }
